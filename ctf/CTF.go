6f28d1bcb3089600605fa64666429dda7de3f934
Patrick Wu <scorpio7500@gmail.com>
2016-04-29 18:28:35 -0400

    almost done with constructor

--- ctf/CTF.go
+++ ctf/CTF.go
@@ -1,54 +1,84 @@
-package ctf
+package main
 
 import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"math/rand"
 	"net/http"
 	"os"
-	"strconv"
-	"time"
 )
 
-type candidate struct {
-	voteCount int []
-	voterIDs string []
+type ctfConfig struct {
+	ClaSecret string
+	CtfSecret string
 }
 
-type voters struct {
-	publicKey string
-	privateKey string
+type candidate struct {
+	name		string
+	voteCount	[]int
+	voterIDs 	[]string
 }
 
 type Ctf struct {
-	var candidates candidate []
-	validationNumbers int []
-	var voters
-	var publicKey
-	var privateKey
+	Config					ctfConfig
+	candidates 				[]candidate
+	validationNumbers 		map[string]bool // validation values will be true if used, false otherwise
+	CandidateNames 			[]string // required to unmarshal json into
+	voters					[]string
 }
 
-func newCtf() (*Ctf, error) {
+// create a new
+func NewCtf(configFileName string) (*Ctf, error) {
 	var ctf Ctf
+
+	candidateFile, err := os.Open(configFileName)
 	if err != nil {
-		fmt.println(os.Stderr, "Error generating CTF")
+		fmt.Fprintln(os.Stderr, "Error opening file", configFileName, ":", err)
+		return nil, err
 	}
-	if err = json.Unmarshal(fileBytes, &cla); err != nil {
+	defer candidateFile.Close()
+
+	fileBytes, err := ioutil.ReadAll(candidateFile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading file", configFileName, ":", err)
+		return nil, err
+	}
+	if err = json.Unmarshal(fileBytes, &ctf); err != nil {
 		fmt.Fprintln(os.Stderr, "Error Unmarshalling data:", err)
 		return nil, err
 	}
-	// request constructor variables from CLA through http
+	// set candidate slice to len of
+	ctf.candidates = make([]candidate, len(ctf.CandidateNames), cap(ctf.CandidateNames))
+	fmt.Println(ctf.CandidateNames)
+	// Get validationNumbers from CLA
+	mapCandidateNames(ctf)
+	return &ctf, nil
 }
 
-func vote(voter, ctf) {
-	// fill stuff here
+func mapCandidateNames(ctf Ctf) {
+	for key, val := range ctf.CandidateNames {
+		// fmt.Println("Key: ", key, "\tVal: ", val)
+		ctf.candidates[key].name = val
+	}
+	// I should destroy ctf.CandidateNames but I'm not sure how
 }
 
-func sendOutcome(voter, ctf) {
-	// fill stuff here
+func votingHandler(w http.ResponseWriter, r *http.Request, ctf *Ctf) {
+
 }
 
-func main {
-	// fill stuff here
+func sendOutcome(ctf Ctf) {
+	// http POST
+}
+
+func main() {
+	ctf, err := NewCtf("ctf/config.json")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: ", err)
+		return
+	}
+	http.HandleFunc("/vote", func(w http.ResponseWriter, r *http.Request) {
+		votingHandler(w, r, ctf)
+	})
+	http.ListenAndServeTLS(":9999", "certs/localhost.crt", "keys/localhost.key", nil)
 }
