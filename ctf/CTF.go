5993224111b9dbad1491915d9a2a91bc89bb2a0d
Patrick Wu <scorpio7500@gmail.com>
2016-05-01 19:59:20 -0400

    added list handler functionality

--- ctf/CTF.go
+++ ctf/CTF.go
@@ -19,14 +19,22 @@ type candidate struct {
 	voterIDs  []string // list of validation numbers who have voted for that particular candidate
 }
 
+type vote struct {
+	CandidateName string
+}
+
+type listRequest struct {
+	SharedSecret string
+	ValidationNums []string
+}
+
 type Ctf struct {
 	Config            ctfConfig
 	candidates        []candidate
-	validationNumbers map[string]bool // validation values will be true if used, false otherwise
-	CandidateNames    []string        // required to unmarshal json into
+	ValidationNums map[string]bool // validation values will be true if used, false otherwise
+	CandidateNames    []string        // required to unmarshal json into candidates
 }
 
-// create a new
 func NewCtf(configFileName string) (*Ctf, error) {
 	var ctf Ctf
 
@@ -49,7 +57,7 @@ func NewCtf(configFileName string) (*Ctf, error) {
 	// set candidate slice to len of the candidate name list
 	ctf.candidates = make([]candidate, len(ctf.CandidateNames), cap(ctf.CandidateNames))
 	mapCandidateNames(ctf)
-	// Get validationNumbers from CLA
+
 	return &ctf, nil
 }
 
@@ -58,12 +66,49 @@ func mapCandidateNames(ctf Ctf) {
 		// fmt.Println("Key: ", key, "\tVal: ", val)
 		ctf.candidates[key].name = val
 	}
-	// I should destroy ctf.CandidateNames but I'm not sure how
+	ctf.CandidateNames = nil
+	return
+}
+
+// grabs validation numbers from /list
+func listHandler(w http.ResponseWriter, r *http.Request, ctf *Ctf) {
+	var args listRequest
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error unpacking user args.", http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(body, &args)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error unmarshalling json!", err)
+		return
+	}
+
+	if args.SharedSecret == "" || cla.Config.CtfSecret != args.SharedSecret {
+		http.Error(w, "Sent shared secret does not belong to the CTF.", http.StatusForbidden)
+		return
+	}
+	for _, validationNum := range args.ValidationNums {
+		ctf.ValidationNums[validationNum] = true
+	}
 }
 
 func votingHandler(w http.ResponseWriter, r *http.Request, ctf *Ctf) {
 	// check if voting allowed by requesting validation numbers if you don't have them yet
 	// get voterID from request body
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error unpacking user args.", http.StatusBadRequest)
+		return
+	}
+
+	err = json.Unmarshal(body, &args)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error unmarshalling json!", err)
+		return
+	}
 
 	// will throw error either when voter ID has been used or doesn't exist
 	if ctf.validationNumbers[voterID] == true {
@@ -77,11 +122,16 @@ func sendOutcome(ctf Ctf) {
 }
 
 func main() {
+
 	ctf, err := NewCtf("ctf/config.json")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: ", err)
 		return
 	}
+	http.HandleFunc("/list", func(w http.ResponseWriter, r *http.Request) {
+		listHandler(w, r, ctf)
+	}
+
 	http.HandleFunc("/vote", func(w http.ResponseWriter, r *http.Request) {
 		votingHandler(w, r, ctf)
 	})
